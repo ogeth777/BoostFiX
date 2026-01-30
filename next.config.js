d47911ae8a8f25702dbdf309b80c684252193600
ogeth777 <ogeth777@gmail.com>
2026-01-30 14:43:26 +0300

    Fix: Resolve pino-pretty build error by ignoring it in webpack config

--- next.config.js
+++ next.config.js
@@ -12,6 +12,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "pino-pretty": false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
